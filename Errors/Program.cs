730dfd17c76d432723e9fe66eafbb18b729cc7f0
İlker Acar <ilkerA76@gmail.com>
2024-12-22 23:21:56 +0300

    second commit for git

---
+++ Errors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Errors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Malesef kolleksiyon diye tanımlanan aslında List, ArrayList ,Array gibi konuların anlatıldığı
+            //Collection solitionu kaydederken donup kapandı ve kurtaramadım hiçbir veriyi.
+
+            // Hataların kontrolü için try catch yapısı kullanılır.
+
+            try
+            {
+                string[] students = new string[3] { "Ahmet", "Mehmet", "Ali" };
+                students[3] = "Ahmet";
+            }
+            catch (NullReferenceException)
+            {
+                //Parantez içerisindeki türde bir hatayı alırsa burayı  yazdıracak.
+                Console.WriteLine("a");
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("b");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata kodu :{0}" , e.Message);
+            }
+
+
+        }
+    }
+}
